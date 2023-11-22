e0916219ff0a6b166ff60e5e9a51db29b66ace2d
José Peralta <joseperalta2910@gmail.com>
2023-11-21 21:32:38 -0400

    Feature: Open movie details added. Feature: Basic routing added.

--- src/js/render.js
+++ src/js/render.js
@@ -1,3 +1,7 @@
+function renderHeader() {
+  document.querySelector(".app-header").classList.remove("inactive");
+}
+
 function renderTrendingMoviesPreview(movies) {
   const movieEL = movies.map(movie => {
     const movieContainer = document.createElement("div");
@@ -7,6 +11,9 @@ function renderTrendingMoviesPreview(movies) {
     movieImage.src = `https://image.tmdb.org/t/p/w300${movie.poster_path}`
 
     movieContainer.append(movieImage);
+
+    movieImage.addEventListener("click", ()=> location.hash = `movie=${movie.id}`);
+
     return movieContainer;
   });
 
@@ -22,10 +29,56 @@ function renderCategories(genres) {
     genreTitle.innerText = genre.name;
 
     genreContainer.append(genreTitle);
+
+    genreContainer.addEventListener("click", ()=> location.hash = `categorie=${genre.name}`)
     return genreContainer;
   })
 
   document.querySelector(".categories_list").append(...categoriesEL);
 }
 
-export { renderTrendingMoviesPreview, renderCategories };
+
+function renderMovie(movie) {
+  const movieEL = {
+    banner: document.querySelector(".banner_image"),
+    poster: document.querySelector(".movie_poster"),
+    title: document.querySelector(".movie_title"),
+    rating: document.querySelector(".movie_rating"),
+    release_date: document.querySelector(".movie_release-date"),
+    duration: document.querySelector(".movie_duration"),
+    genres: document.querySelector(".movie_genres"),
+    overview: document.querySelector(".movie_overview")
+  }
+  
+  movieEL.banner.src = `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+  movieEL.title.innerText = `${movie.original_title} (${movie.release_date.slice(0,4)})`;
+  movieEL.rating.innerText = movie.vote_average.toFixed(1)+ ` ⭐`;
+  movieEL.duration.innerText = movie.runtime;
+  movie.genres.forEach(genre => {
+    let element = document.createElement('p');
+    element.innerText = genre.name;
+    element.classList.add('genre');
+    movieEL.genres.append(element);
+  });
+  movieEL.overview.innerText = movie.overview;
+
+  document.querySelector(".trending").classList.add('inactive');
+  document.querySelector(".categories").classList.add('inactive');
+  document.querySelector(".movie").classList.remove('inactive');
+  document.querySelector(".app-header").classList.add("inactive");
+}
+
+function renderHomePage(movies, genres) {
+  if(document.querySelector(".trending").classList.contains('inactive')) {
+    document.querySelector(".trending").classList.remove('inactive');
+    document.querySelector(".categories").classList.remove('inactive');
+    document.querySelector(".app-header").classList.remove("inactive");
+  } else {
+    renderHeader();
+    renderTrendingMoviesPreview(movies);
+    renderCategories(genres);
+  }
+  document.querySelector(".movie").classList.add('inactive');
+}
+
+export { renderHomePage, renderMovie };
--- src/main.js
+++ src/main.js
@@ -1,16 +1,66 @@
 import { fetchInfo } from "./js/apiHandler.js";
-import { renderTrendingMoviesPreview, renderCategories } from "./js/render.js";
+import { renderHomePage, renderMovie } from "./js/render.js";
 
 const ENDPOINTS = {
   trending: "/trending/movie/day",
-  genres: "/genre/movie/list"
+  genres: "/genre/movie/list",
+  movie: "/movie/"
+}
+
+async function getMovie() {
+  const  re = /=[0-9]*$/g;
+  const  movieid = location.hash.replace("#", "").match(re)[0].replace("=","");
+  const movie =  await fetchInfo(`${ENDPOINTS.movie}/${movieid}`);
+  return movie;
+}
+
+function navigator() {
+  let route;
+  if(location.hash === '') route = "home";
+  else if(location.hash.replace("#", "").startsWith("movie")) route = "movie";
+  else if(location.hash.replace("#", "").startsWith("categorie")) route = "categorie";
+  else if(location.hash.replace("#", "").startsWith("search")) route = "search"
+  return route;
+}
+
+async function render() {
+  if(navigator() === 'home') {
+    const movies = await fetchInfo(ENDPOINTS.trending);
+    const genres = await fetchInfo(ENDPOINTS.genres);
+    renderHomePage(movies.results, genres.genres);
+  } else if(navigator() === "movie") {
+    renderMovie(await getMovie());
+  }
 }
 
 async function app() {
-  const movies = await fetchInfo(ENDPOINTS.trending);
-  const genres = await fetchInfo(ENDPOINTS.genres);
-  renderTrendingMoviesPreview(movies.results);
-  renderCategories(genres.genres);
+  const app = {
+    header: {
+      title: document.querySelector('.app-title'),
+      searchBar: document.querySelector(".search_bar"),
+      searchButton: document.querySelector(".search_button")
+    },
+    body: {
+      viewMoreButton: document.querySelector(".trending_view-more")
+    }
+  };
+
+  await render();
+
+  window.addEventListener('hashchange', async ()=>{
+    await render();
+  });
+
+  app.header.title
+    .addEventListener('click', ()=>location.hash = "");
+  app.header.searchButton
+    .addEventListener('click', ()=>{
+      if(app.header.searchBar.value.trim() !== "") location.hash = `search=${app.header.searchBar.value.trim()}`;
+    });
+  app.body.viewMoreButton
+    .addEventListener('click', ()=>location.hash = "trendings");
+  document.querySelector(".movie .back_button")
+    .addEventListener("click", ()=>{location.hash = ""});
 }
 
 app();
